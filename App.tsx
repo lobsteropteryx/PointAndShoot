49e00e730e0f8747231128348f6d4746dfca5a53
Ian Firkin <ian.firkin@gmail.com>
2022-03-16 20:31:02 -0400

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [location, setLocation] = useState({ coords: {} });
+  const [text, setText] = useState('');
+  const [waypointEnabled, setWaypointEnabled] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setText('Permission to access location was denied');
+      } else {
+        setWaypointEnabled(true);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{text}</Text>
+      <Button disabled={!waypointEnabled} title={"Capture Waypoint"} onPress={ async () => {
+         let location = await Location.getCurrentPositionAsync({});
+         setLocation(location);
+         setText( `${location.coords.latitude}, ${location.coords.longitude}, ${location.coords.heading}`) 
+      }} />
     </View>
   );
 }
